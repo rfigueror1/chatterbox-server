228bffb94cfdc3b5fe9ba298ecbfbef02566a606
rfigueror1 <rfigueror@gmail.com>
2018-07-02 18:20:26 -0700

    passed tests for GET requests

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,4 +1,6 @@
 /* Import node's http module: */
+
+var handleRequest = require('./request-handler');
 var http = require('http');
 
 
@@ -15,16 +17,16 @@ var port = 3000;
 var ip = '127.0.0.1';
 
 
-
 // We use node's http module to create a server.
 //
 // The function we pass to http.createServer will be used to handle all
 // incoming requests.
 //
 // After creating the server, we will tell it to listen on the given port and IP. */
-var server = http.createServer(handleRequest);
+var server = http.createServer(handleRequest.requestHandler);
 console.log('Listening on http://' + ip + ':' + port);
 server.listen(port, ip);
+
 
 // To start this server, run:
 //
